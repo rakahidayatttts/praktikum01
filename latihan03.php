6d4d11c7d8aa006e865d942a6957517527811f3b
rakahidayatttts <100068238+rakahidayatttts@users.noreply.github.com>
2022-04-27 23:27:27 +0700

    Add files via upload

---
+++ latihan03.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="latihan01a.css">
+  <title>Latihan 03 - PHP dan File</title>
+</head>
+<body>
+<?php
+    // baca data dari file .csv
+    $file = fopen("klasemen.csv","r");
+    while (($data_row = fgetcsv($file, 1000, ",")) !== FALSE) {
+        // simpan ke dalam array 2 dimensi
+        $racer_list[] = $data_row;
+    }
+    // DEBUG
+    // print_r($racer_list);
+    fclose($file)
+?>
+    <h1>Klasemen Sementara (HTML + PHP + CSV File)</h1>
+    <table class="styled-table">
+        <thead>
+            <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Points</th>
+                <th>Team</th>
+                <th>Country</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+# tulis isi array ke page
+foreach ($racer_list as $racer) {
+    echo ("<tr><td>" . $racer[0] . "</td><td>" . $racer[1] . "</td><td>" . $racer[2] . "</td><td>" . $racer[3] .  "</td><td>" . $racer[4]);
+  }
+
+?>
+</tbody>
+</table>
+</body>
+</html>
---
+++ latihan04a.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE-edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Query ke Database</title>
+  <link rel="stylesheet" href="latihan01a.css">
+  
+</head>
+<body>
+    <h1>Klasemen Sementara (HTML + PHP + MySQL)</h1>
+    <?php
+    // database credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "praktikum01";
+
+    // Create connection to database
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check if connection is succesful
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT rank, name, points, team
+    FROM klasemen";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0){
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "rank: " . $row["rank"]. " - Name: " . $row["name"].
+        " " . $row["points"]. " - Team: " . $row['team'] . "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+    $conn->close();
+    ?>
+  </body>
+  </html>
